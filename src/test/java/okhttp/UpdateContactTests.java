bcedf0135cfe015044ed66a8dd87efb3c36182ad
MikeTestin9 <Michael_benin@hotmail.com>
2023-08-07 00:12:13 +0300

    HW19

--- src/test/java/okhttp/UpdateContactTests.java
+++ src/test/java/okhttp/UpdateContactTests.java
@@ -3,22 +3,20 @@ package okhttp;
 import dto.ContactDTO;
 import dto.ContactResponseDTO;
 import helpers.Helper;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import okhttp3.*;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-public class DeleteContactByIDTests implements Helper {
-
-    String endpoint = "contacts";
+public class UpdateContactTests implements Helper {
+    ContactDTO contactDTO;
     String id;
+    String endpoint = "contacts";
 
     @BeforeMethod
-    public void precondition() throws IOException {
+    public void precondition() throws IOException, IOException {
 
         ContactDTO contactDTO = ContactDTO.builder()
                 .name("QA38")
@@ -39,7 +37,6 @@ public class DeleteContactByIDTests implements Helper {
 
         Response response = client.newCall(request).execute();
 
-
         ContactResponseDTO contactResponseDTO = gson.fromJson(response.body().string(), ContactResponseDTO.class);
 
         Assert.assertTrue(response.isSuccessful());
@@ -49,20 +46,27 @@ public class DeleteContactByIDTests implements Helper {
     }
 
     @Test
-    public void deleteContactByIDPositive() throws IOException {
+    public void updateContactPositive() throws IOException {
+
+        String newContactName = "QA_38_Updated";
+        contactDTO.setName(newContactName);
+
+        RequestBody requestBody = RequestBody.create(gson.toJson(contactDTO), JSON);
 
         Request request = new Request.Builder()
-                .url(BASE_URI + "/" + PATH + "/" + endpoint+ "/" + id)
-                .addHeader(authHeader, TOKEN)
-                .delete()
+                .url(BASE_URI + "/" + PATH + "/" + endpoint + "/" + id)
+                .header(authHeader, TOKEN)
+                .patch(requestBody)
                 .build();
 
         Response response = client.newCall(request).execute();
 
         ContactResponseDTO contactResponseDTO = gson.fromJson(response.body().string(), ContactResponseDTO.class);
 
-        Assert.assertTrue(response.isSuccessful());
-        String message = contactResponseDTO.getMessage();
-        System.out.println(message);
+
+        Assert.assertEquals(200, response.code());
+        Assert.assertEquals("Contact was updated", contactResponseDTO.getMessage());
+
     }
+
 }
